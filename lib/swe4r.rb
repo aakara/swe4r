7f034073d399bdfb5b4ea498ef5c87698b3b42f1
Andrew Kirk <andrew.kirk@windhorsemedia.com>
2012-05-14 11:01:35 -0600

    Removed Swe4r include

--- lib/swe4r.rb
+++ lib/swe4r.rb
@@ -1,2 +1,19 @@
+# Swe4r :: Swiss Ephemeris for Ruby - A C extension for the Swiss Ephemeris library (http://www.astro.com/swisseph/)
+# Copyright (C) 2012 Andrew Kirk (andrew.kirk@windhorsemedia.com)
+# 
+# This file is part of Swe4r.
+# 
+# Swe4r is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Swe4r is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Swe4r.  If not, see <http://www.gnu.org/licenses/>.
+
 require 'swe4r/swe4r'
-include Swe4r
